2081aa164d83b420e7aa44d466addd9b10265e6a
Heather Frantisak <hfrantis@my.hpu.edu>
2014-04-14 02:34:06 +0000

    Initial commit scraper

---
+++ app/models/quote_scraper.rb
@@ -0,0 +1,22 @@
+class QuoteScraper
+	def self.scrape
+		# Use the class methods to get down to business quickly
+		response = HTTParty.get('http://www.brainyquote.com/quotes/authors/o/oscar_wilde.html')
+
+
+		doc = Nokogiri::HTML(response.body)
+
+		# Do funky things with it using Nokogiri::XML::Node methods...
+
+		####
+		# Search for nodes by css
+		
+		quotes = []
+		doc.css('.bqQuoteLink a').each do |quote|
+			quotes << quote.inner_html
+		end
+
+		quotes.sample
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scraper::Application.config.secret_key_base = 'c6d8d237e9e9f34dd2be3ac40ed434261993e5c6f1486ba70934af4ae71520f3a2f113b6ffe452e64766dc35c40e97c5d2aef23fee2201b54916f19c92c400d6'
